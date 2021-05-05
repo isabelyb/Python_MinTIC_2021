2f39e019bf54147ffeb8031d75edfda70461a8e2
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-04 21:47:21 -0500

    challenge1 finished

--- week_1/reto1/reto1.py
+++ week_1/reto1/reto1.py
@@ -1,7 +1,7 @@
 
 
 def value_to_pay(name, last_name, value_insured, discount):
-    print(f"The {name} {last_name}'s insurance value is: $ {sale_value_insurance} and the discount received is: $ {discount}")
+    print(f"The {name} {last_name}'s insurance value to pay is: $ {sale_value_insurance} and the discount received is: $ {discount}")
 
 '''
 La función debe retornar una cadena de caracteres que le proporcione al asesor de seguros el
@@ -9,16 +9,20 @@ valor que debe pagar el tomador de acuerdo con el valor asegurado y el descuento
 por la promoción del mes.
 '''
 
-name = 'Pedro'
-last_name = 'Díaz'
-value_insured = x = 10000000
-discount = 5
+name = input("name: ")
+last_name = input("last_name: ")
+value_insured = int(input("type the value to insure: "))  # if the value is how the challenge, please type 5.
+discount = int(input("How much is the discount to applicate?  "))
 
-value_insurance = (x*15)/100
+value_insurance = (value_insured*15)/100
 discount_value = (value_insurance* discount) /100
 sale_value_insurance = value_insurance - discount_value
 
 value_to_pay(name, last_name, value_insured, discount_value)
+
+
+
+
 
 
 
