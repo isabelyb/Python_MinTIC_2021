7531078657d8d1cbe87f47e28adc13bb11eb45db
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-05 22:02:13 -0500

    more functions

---
+++ week_1/functions.py
@@ -0,0 +1,14 @@
+
+def sustraction(a, b):
+    result0 = a - b
+    return print(result0)
+
+def adition(a , b, c):
+    result1 = a + b + c
+    return print(result1)
+
+def adition_sustraction():
+    adition(10, 20, 40)
+    sustraction(100, 50)
+
+adition_sustraction()
