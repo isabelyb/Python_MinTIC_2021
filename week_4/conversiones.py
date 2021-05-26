fdff0fb04a32d4a513fe77f0cf2213db63b1285d
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-26 08:25:12 -0500

    conversiones unidad4

---
+++ week_4/conversiones.py
@@ -0,0 +1,14 @@
+lista = ['1','2','3','4','5']
+print(lista)
+lista1 = "".join(lista)
+print(lista1)
+tupla = tuple(lista)
+print(tupla)
+conjunto = set(lista)
+print(conjunto)
+
+
+tupla1 = (1,2,3,4,5,6,7,8,9,10)
+cadena = "".join(str(i) for i in tupla1)
+print(cadena)
+
