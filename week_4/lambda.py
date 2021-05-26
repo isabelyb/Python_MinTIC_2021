ed45ff35376ab401a2aaae579b006fbcb72eb221
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-26 10:17:25 -0500

    colecciones unidad 4

--- week_4/lambda.py
+++ week_4/lambda.py
@@ -7,3 +7,11 @@ operacion = lambda operacion, val1=0, val2=0  : operacion(val1, val2)
 resultado = operacion(suma, 10, 20)
 
 print(resultado)
+
+
+z = lambda a,b,c: a + b + c
+print(z(1,2,3))
+
+
+suma = lambda *args: sum(args)
+print(suma(3,4,5,6,7))
