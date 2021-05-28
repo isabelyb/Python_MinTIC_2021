1dc852dc051cde59bd986a3bfa6ab1dc34dde038
isabelyb <isabelcristinayepesb@gmail.com>
2021-05-28 15:45:00 -0500

    numpy y pandas 2021-05-28 15:44

---
+++ week_4/lib_numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+x = np.array([0,10,2])
+print(x)
+
+
+from numpy import random as r 
+
+print(r.choice(['Andres', 'Juan', 'Pedro', 'Mateo']))
+
+matriz_random = np.random.random((10,10)) # Matriz de datos aleatorios
+print(matriz_random)
